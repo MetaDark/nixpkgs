825ae548f9b6705b125e1cfd6292ce7e4ecdfc76
MetaDark <kira.bruneau@gmail.com>
2019-07-10 21:55:48 -0400

    rls: 1.34.0 -> 1.35.0

--- pkgs/development/tools/rust/rls/default.nix
+++ pkgs/development/tools/rust/rls/default.nix
@@ -1,28 +1,28 @@
 { stdenv, fetchFromGitHub, rustPlatform
-, openssh, openssl, pkgconfig, cmake, zlib, curl, libiconv }:
+, openssl, pkgconfig, cmake, zlib }:
 
 rustPlatform.buildRustPackage rec {
   name = "rls-${version}";
   # with rust 1.x you can only build rls version 1.x.y
-  version = "1.34.0";
+  version = "1.35.0";
 
   src = fetchFromGitHub {
     owner = "rust-lang";
     repo = "rls";
-    rev = "0d6f53e1a4adbaf7d83cdc0cb54720203fcb522e";
-    sha256 = "1aabs0kr87sp68n9893im5wz21dicip9ixir9a9l56nis4qxpm7i";
+    rev = version;
+    sha256 = "1l3fvlgfzri8954nbwqxqghjy5wa8p1aiml12r1lqs92dh0g192f";
   };
 
-  cargoSha256 = "16r9rmjhb0dbdgx9qf740nsckjazz4z663vaajw5z9i4qh0jsy18";
+  cargoSha256 = "0v96ndys6bv5dfjg01chrqrqjc57qqfjw40n6vppi9bpw0f6wkf5";
 
   # a nightly compiler is required unless we use this cheat code.
-  RUSTC_BOOTSTRAP=1;
+  RUSTC_BOOTSTRAP = 1;
 
   # clippy is hard to build with stable rust so we disable clippy lints
   cargoBuildFlags = [ "--no-default-features" ];
 
   nativeBuildInputs = [ pkgconfig cmake ];
-  buildInputs = [ openssh openssl curl zlib libiconv ];
+  buildInputs = [ openssl zlib ];
 
   doCheck = true;
   # the default checkPhase has no way to pass --no-default-features
